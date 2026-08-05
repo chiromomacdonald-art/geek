3940b9f9d612cf3cb9f6fe89ca37d1e58e0a82d2
chiromomacdonald-art <chiromomacdonald@gmail.com>
2026-08-05 19:04:11 +0200

    initial commit
    commit 2

--- admin.js
+++ admin.js
@@ -1,9 +1,8 @@
 
-import {connectDB} from "../../lib/db";
-import Job from "../../models/Job";
-
-export default async function handler(req,res){
- await connectDB();
- const jobs=await Job.find().sort({createdAt:-1});
- res.json(jobs);
-}
+import mongoose from "mongoose";
+const JobSchema = new mongoose.Schema({
+ title:String,
+ link:String,
+ createdAt:{type:Date, default:Date.now}
+});
+export default mongoose.models.Job || mongoose.model("Job", JobSchema);
--- jobs.js
+++ jobs.js
@@ -1,28 +1,18 @@
 
-import axios from "axios";
-import cheerio from "cheerio";
-import {connectDB} from "../../lib/db";
-import Job from "../../models/Job";
-
-const KEYWORDS=["agriculture","economics","statistics","agribusiness","business"];
-
-export default async function handler(req,res){
- await connectDB();
- const {data}=await axios.get("https://jobsearchmalawi.com/");
- const $=cheerio.load(data);
- let jobs=[];
-
- $(".job-listing").each((i,el)=>{
-  const title=$(el).find(".job-title").text();
-  const link=$(el).find("a").attr("href");
-  if(KEYWORDS.some(k=>title.toLowerCase().includes(k))){
-    jobs.push({title,link});
-  }
- });
-
- for(let job of jobs){
-  await Job.updateOne({link:job.link},{ $set:job},{upsert:true});
- }
-
- res.json({count:jobs.length});
+import {useEffect,useState} from "react";
+export default function Home(){
+ const[jobs,setJobs]=useState([]);
+ useEffect(()=>{
+  fetch("/api/jobs").then(r=>r.json()).then(setJobs);
+ },[]);
+ return(
+ <div style={{padding:40}}>
+ <h1>Job Dashboard</h1>
+ {jobs.map((j,i)=>(
+  <div key={i}>
+   <h3>{j.title}</h3>
+   <a href={j.link}>View</a>
+  </div>
+ ))}
+ </div>);
 }

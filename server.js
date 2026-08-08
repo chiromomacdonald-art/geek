f40d5ab236c5d5a45a617c1ee2f83efc9eaa4646
chiromomacdonald-art <chiromomacdonald@gmail.com>
2026-08-08 14:28:42 +0200

    initial commit
    commit 3

---
+++ server.js
@@ -0,0 +1,19 @@
+{
+  "name": "agrijobhub-malawi",
+  "version": "1.0.0",
+  "description": "AgriJobHub Malawi - job board for agriculture, economics, agribusiness, and business administration roles in Malawi.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "dependencies": {
+    "express": "^4.19.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.4"
+  }
+}
